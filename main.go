f3f2e12bffbaff5d543cdf58ed92a0c64fbd224c
Gopal Singh <gopalkhasria@users.noreply.github.com>
2020-05-29 21:20:54 +0200

    Second try

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bitcointransaction/controllers"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", controllers.HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
